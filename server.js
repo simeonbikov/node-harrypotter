423549e89e2a967c2005cbd6b6a3c76d142cec57
simeonbikov <bikovsem@gmail.com>
2022-12-19 23:22:26 +0000

    les.2

--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-
-
 /* 
 We have a client requirement to build a RESTful API in Express
 to manage a database of Harry Potter Characters.
@@ -22,39 +20,172 @@ This API should include:
 /characters?house=Gryffindor
 /characters?house=Slytherin
 
+
+C - Create
+  - POST request
+
+R - Read/Retrieve
+  - GET request
+
+U - Update
+  - PUT
+    - Updating the entire document
+
+  Original Document
+  {
+    "name": "Hannah Abbott",
+    "house": "Hufflepuff"
+  }
+  
+  Hannah Abbot changes house to Gryffindor
+
+  PUT /characters/22
+  Body:
+    {
+      "name": "Hannah Abbott",
+      "house": "Gryffindor"
+    }
+
+  PATCH /characters/22
+
+  Body:
+    {
+      "house": "Gryffindor"
+    }
+
+
+D - Delete
+  - DELETE
+  
+  DELETE /characters/:id
+
+
+POST /characters   
+
+  Headers
+    {
+      "Authentication": <auth token>
+    }
+
+  Body: 
+    {
+      "name": "Hannah Abbott",
+      "house": "Hufflepuff"
+    }
+
+
+// 200 OK 
+// 400 Client Error (bad input/no auth/wrong auth)
+// 500 Server Error (API broke)
+
+
+
 */
 
 
-// import express
-const express = require("express")
-const fs = require("fs")
+const express = require("express");
+const fs = require("fs");
 
-// create a new express api
-const app = express()
+const app = express();
+app.use(express.json());
 
-const port = 3000
+const port = 3000;
 
-const data = JSON.parse(fs.readFileSync('data.json', 'utf-8'));
+let data = JSON.parse(fs.readFileSync("data.json", "utf-8"));
+let maxID = Math.max(...data.map((c) => c.id));
 
 app.get("/characters", (req, res) => {
-    let characters = data
+  let characters = data;
 
-    if (req.query.name) {
-        characters = characters.filter(c => c.name === req.query.name)
-    }
+  if (req.query.name) {
+    characters = characters.filter((c) => c.name === req.query.name);
+  }
 
-    if (req.query.house) {
-        characters = characters.filter(c => c.house === req.query.house)
-    }
+  if (req.query.house) {
+    characters = characters.filter((c) => c.house === req.query.house);
+  }
 
-    res.json(characters)
-})
+  res.json(characters);
+});
 
 app.get("/characters/:id", (req, res) => {
-    const characterId = parseInt(req.params.id)
-    res.json(data.find(c => c.id === characterId))
-})
+  const characterId = parseInt(req.params.id);
+  res.json(data.find((c) => c.id === characterId));
+});
+
+app.post("/characters", (req, res) => {
+  if (!req.body.name || !req.body.house) {
+    res.status(400).send("You must include a name and a house in your request");
+    return;
+  }
+  const validHouses = ["Gryffindor", "Ravenclaw", "Slytherin", "Hufflepuff"];
+  if (!validHouses.includes(req.body.house)) {
+    res.status(400).send("House must be a valid Hogwarts House");
+    return;
+  }
+
+  const newCharacter = {
+    id: ++maxID,
+    name: req.body.name,
+    house: req.body.house,
+  };
+
+  data.push(newCharacter);
+  save();
+  res.json(newCharacter);
+});
+
+app.delete("/characters/:id", (req, res) => {
+
+  const characterId = parseInt(req.params.id);
+  const characterIndex = data.findIndex((c) => c.id === characterId);
+
+  if (characterIndex < 0) {
+    res.sendStatus(404);
+    return;
+  }
+
+  data.splice(characterIndex, 1);
+  save();
+  res.send("character deleted");
+});
+
+app.put("/characters/:id", (req, res) => {
+
+  if (!req.body.name || !req.body.house) {
+    res.status(400).send("You must include a name and a house in your request");
+    return;
+  }
+  const validHouses = ["Gryffindor", "Ravenclaw", "Slytherin", "Hufflepuff"];
+  if (!validHouses.includes(req.body.house)) {
+    res.status(400).send("House must be a valid Hogwarts House");
+    return;
+  }
+
+  const characterId = parseInt(req.params.id);
+  const characterIndex = data.findIndex((c) => c.id === characterId);
+
+  if (characterIndex < 0) {
+    res.sendStatus(404);
+    return;
+  }
+
+  const updatedCharacter = {
+    id: characterId,
+    name: req.body.name,
+    house: req.body.house,
+  };
+
+  const origCharacter = data[characterIndex];
+  data[characterIndex] = updatedCharacter;
+  save();
+  res.json([origCharacter, updatedCharacter]);
+});
+
+const save = () => {
+  fs.writeFileSync("data.json", JSON.stringify(data, null, 2));
+};
 
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`)
-})
+  console.log(`Listening on port ${port}`);
+});
