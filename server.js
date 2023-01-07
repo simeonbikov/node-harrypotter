fa0816bca90f9763e0a7501d3a7a9423394b829d
simeonbikov <bikovsem@gmail.com>
2023-01-07 15:59:07 +0000

    refactoring

--- server.js
+++ server.js
@@ -82,18 +82,21 @@ POST /characters
 
 */
 
-
+// import express
 const express = require("express");
 const fs = require("fs");
 
+// create a new express api
 const app = express();
 app.use(express.json());
 
 const port = 3000;
 
+//read in from the json file
 let data = JSON.parse(fs.readFileSync("data.json", "utf-8"));
 let maxID = Math.max(...data.map((c) => c.id));
 
+// READ ENDPOINTS
 app.get("/characters", (req, res) => {
   let characters = data;
 
@@ -113,7 +116,13 @@ app.get("/characters/:id", (req, res) => {
   res.json(data.find((c) => c.id === characterId));
 });
 
+// END READ ENDPOINTS
+
+// CREATE ENDPOINT
 app.post("/characters", (req, res) => {
+  // 1) validate our input
+  // make sure house and name are present
+  // make sure that house is a valid Hogwarts house
   if (!req.body.name || !req.body.house) {
     res.status(400).send("You must include a name and a house in your request");
     return;
@@ -123,6 +132,8 @@ app.post("/characters", (req, res) => {
     res.status(400).send("House must be a valid Hogwarts House");
     return;
   }
+
+  // 2) format/build our new resource
 
   const newCharacter = {
     id: ++maxID,
@@ -130,12 +141,17 @@ app.post("/characters", (req, res) => {
     house: req.body.house,
   };
 
+  // 3) save our new resource
+
   data.push(newCharacter);
   save();
   res.json(newCharacter);
 });
+// END CREATE ENDPOINT
 
 app.delete("/characters/:id", (req, res) => {
+  // 1) Validate your input
+  // check the id is valid and that the resource exists
 
   const characterId = parseInt(req.params.id);
   const characterIndex = data.findIndex((c) => c.id === characterId);
@@ -145,13 +161,21 @@ app.delete("/characters/:id", (req, res) => {
     return;
   }
 
+  // 2) Delete the object
+  //     save the data
+
   data.splice(characterIndex, 1);
   save();
   res.send("character deleted");
 });
 
-app.put("/characters/:id", (req, res) => {
+//PUT
 
+app.put("/characters/:id", (req, res) => {
+  //1) Validate your input
+  // check the character exists
+
+  // check contents of body
   if (!req.body.name || !req.body.house) {
     res.status(400).send("You must include a name and a house in your request");
     return;
@@ -162,6 +186,7 @@ app.put("/characters/:id", (req, res) => {
     return;
   }
 
+  // check character resource exists
   const characterId = parseInt(req.params.id);
   const characterIndex = data.findIndex((c) => c.id === characterId);
 
@@ -170,22 +195,53 @@ app.put("/characters/:id", (req, res) => {
     return;
   }
 
+  //2) Create an updated document
   const updatedCharacter = {
     id: characterId,
     name: req.body.name,
     house: req.body.house,
   };
 
+  //3) Replace the old document with the new one
   const origCharacter = data[characterIndex];
   data[characterIndex] = updatedCharacter;
   save();
+  // send original and updated, so you can verify the change
   res.json([origCharacter, updatedCharacter]);
 });
 
+/*
+some general thoughts. 
+break your request handlers into reusable functions for ease of
+readability and testing. A sample update request handler might look like:
+{
+  validatebody(req.body.name, req.body.house)
+  const charIndex = checkCharacterExists(req.params.id)
+  if (charIndex<0){
+    res.sendStatus(404)
+  }
+  const updatedChar = buildCharacter(req.body.name, req.body.house, req.params.id)
+  update(updatedChar, charIndex)
+}
+
+testing hierarchy
+// unit < integration < end2end
+
+only small functions can be unit tested properly
+*/
+
+/*
+  This is terrible code - never do this in production
+  It will make your tech lead sad.
+*/
 const save = () => {
   fs.writeFileSync("data.json", JSON.stringify(data, null, 2));
 };
 
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`Listening on port ${port}`);
+// });
+
+app.listen(process.env.PORT || 3000, () =>
+  console.log(`Server started on port: ${port}`)
+);
